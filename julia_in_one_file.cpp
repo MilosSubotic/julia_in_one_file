d6a710008e13e8e25536ee0abfa7dfe7079c24d6
Milos Subotic <milos.subotic.sm@gmail.com>
2014-10-14 22:55:18 +0200

    Better Windows support.

--- julia_in_one_file.cpp
+++ julia_in_one_file.cpp
@@ -18,6 +18,7 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <algorithm>
 #include <stdint.h>
 #include <cstdlib>
 #include <cstring>
@@ -31,9 +32,9 @@ using namespace std;
 // User configuration.
 
 #if defined(WIN32)
-#define OUTPUT_DIR "C:"
+#define DEFAULT_OUTPUT_DIR "C:"
 #else
-#define OUTPUT_DIR "/tmp"
+#define DEFAULT_OUTPUT_DIR "/tmp"
 #endif
 #define BLOCK_SIZE 4096
 
@@ -42,19 +43,87 @@ using namespace std;
 // Initialized with magic number, so it could be changed by script.
 volatile uint64_t tarball_offset = 0xdeadbeefdeadbeef;
 
+///////////////////////////////////////////////////////////////////////////////
+// Path stuff.
+
+namespace os {
+	namespace path {
+
+#if defined(WIN32)
+		const char pathsep =  '\\';
+		const char other_pathsep =  '/';
+#else
+		const char pathsep =  '\\';
+		const char other_pathsep =  '/';
+#endif
+	
+
+		std::string join(const std::string& s0, const std::string& s1) {
+			std::string r(s0);
+			if(!r.empty()){
+				// Last char is not /?
+				if(*(r.end()-1) != pathsep){
+					// Add it.
+					r += pathsep;
+				}
+			}
+			r += s1;
+			return r;
+		}
+
+		static bool doubleSlash(char a, char b){
+			return a == pathsep && b == pathsep;
+		}
+
+		std::string normpath(const std::string& s) {
+			std::string r(s);
+
+			// A\B to A/B
+			replace(r.begin(), r.end(), other_pathsep, pathsep);
+
+			// A//B to A/B
+			r.erase(
+				std::unique(r.begin(), r.end(), doubleSlash),
+				r.end()
+			);
+
+			// A/B/ to A/B
+			if(!r.empty()){
+				// Last char is /?
+				if(*(r.end()-1) == '/'){
+					// Erase it.
+					r.erase(r.size() - 1);
+				}
+			}
+
+			// TODO  A/foo/../B to A/B
+			return r;
+		}
+		
+	} // namespace path
+} // namespace os
+
+///////////////////////////////////////////////////////////////////////////////
+
+std::string path_join(const std::string& s0, const std::string& s1) {
+	return os::path::normpath(os::path::join(s0, s1));
+}
+
 int main(int argc, char** argv) {
 
-	string s = OUTPUT_DIR;
-#if defined(WIN32)
-	s += "\\julia_root\\.status";
-#else
-	s += "/julia_root/.status";
-#endif
-	const char* status_file = s.c_str();
+	string output_dir = DEFAULT_OUTPUT_DIR;
 	
+#if defined(WIN32)
+	const char* env = getenv("LOCALAPPDATA");
+	if(env){
+		output_dir = path_join(env, "Temp");
+	}
+#endif
+
+	string status_file = path_join(output_dir, "julia_root/.status");
 	
 	// If there is no status file unpack julia.
-	if(access(status_file, R_OK)){
+	if(access(status_file.c_str(), R_OK)){
 		const char* elf_path;
 
 #if defined(WIN32)
@@ -111,31 +180,25 @@ int main(int argc, char** argv) {
 		uint32_t tarball_size_in_blocks = ceil(double(tarball_size) / BLOCK_SIZE);
 
 		ostringstream oss;
-		oss << "rm -rf \"" << OUTPUT_DIR 
-#if defined(WIN32)
-			<< "\\julia_root"
-#else
-			<< "/julia_root"
-#endif
-			<< "\" && "
+		oss << "rm -rf \"" << path_join(output_dir, "julia_root") << "\" && "
 			<< "dd bs=" << BLOCK_SIZE
 			<< " count=" << tarball_size_in_blocks
 			<< " skip=" << tarball_offset_in_blocks
 			<< " if=" << elf_path << " | "
-			<< "tar xfvj - -C \"" << OUTPUT_DIR << "\"";
+			<< "tar xfvj - -C \"" << output_dir << "\"";
 	
 		cout << "julia_in_one_file: Unpacking Julia to \"" 
-			<< OUTPUT_DIR << "\"..." << endl;
+			<< output_dir << "\"..." << endl;
 
 		int ret = system(oss.str().c_str());
 		if(ret){
 			cerr << "julia_in_one_file: Error while unpacking Julia to \"" 
-				<< OUTPUT_DIR << "\"!" << endl;
+				<< output_dir << "\"!" << endl;
 			return 1;
 		}
 
 		// Make status file after succesfull unpacking.
-		ofstream sf(status_file);
+		ofstream sf(status_file.c_str());
 		if(!sf.is_open()){
 			cerr << "julia_in_one_file: Cannot open status file \""
 					<< status_file << "\"!" << endl;
@@ -148,12 +211,9 @@ int main(int argc, char** argv) {
 		cout << "julia_in_one_file: Julia succesfully unpacked!" << endl;
 	}
 
-	string julia = OUTPUT_DIR;
-	
+	string julia = path_join(output_dir, "/julia_root/bin/julia");
 #if defined(WIN32)
-	julia += "\\julia_root\\bin\\julia.exe";
-#else
-	julia += "/julia_root/bin/julia";
+	julia += ".exe";
 #endif
 
 	if(execvp(julia.c_str(), argv)){
